9ddcae3f90bf1f642d9b91b4c1f1a4d56b16dde2
darren <darrentran001@gmail.com>
2023-09-16 01:48:04 -0700

    implemented contact form, mailto link used instead while mailserver is down

--- src/app/components/FormContact.tsx
+++ src/app/components/FormContact.tsx
@@ -1,34 +1,74 @@
 "use client";
 
-import { Button, FormControl, FormErrorMessage, HStack, Input, Stack, Textarea, useToast } from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  HStack,
+  Icon,
+  Input,
+  Popover,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverTrigger,
+  Stack,
+  Textarea,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import { EnvelopeClosedIcon } from "@radix-ui/react-icons";
 import { FormEvent, useEffect, useState } from "react";
 
-export default function FormContact({ onCancel }: { onCancel: () => void }) {
+export default function FormContact() {
+  const { onOpen, onClose, isOpen } = useDisclosure();
   const [nameInput, setNameInput] = useState("");
   const [emailInput, setEmailInput] = useState("");
   const [bodyInput, setBodyInput] = useState("");
   const [validEmail, setValidEmail] = useState(true);
+  const [emailSending, setEmailSending] = useState(false);
   const toast = useToast();
 
   const handleCancel = () => {
     setNameInput("");
     setEmailInput("");
     setBodyInput("");
-    onCancel();
+    onClose();
   };
 
-  // TODO: implement sending email
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (validEmail) {
+    if (!validEmail) return;
+    setEmailSending(true);
+    const response = await fetch("https://formsubmit.co/ajax/darrentran001@gmail.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: { nameInput },
+        message: `Email: ${emailInput}\n ${bodyInput}`,
+      }),
+    });
+    setEmailSending(false);
+    if (!response.ok) {
+      toast({
+        title: "Error!",
+        description: "There was an error sending your email",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+      });
+    } else {
       setNameInput("");
       setEmailInput("");
       setBodyInput("");
-      onCancel();
+      onClose();
       toast({
         title: "Email sent!",
         status: "success",
-        duration: 1500,
+        duration: 2000,
         isClosable: true,
       });
     }
@@ -41,37 +81,47 @@ export default function FormContact({ onCancel }: { onCancel: () => void }) {
   }, [emailInput]);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Stack spacing={4} p={2}>
-        <Input
-          id="name"
-          placeholder="Name"
-          isRequired
-          value={nameInput}
-          onChange={(e) => setNameInput(e.target.value)}
-        />
-        <FormControl isInvalid={!validEmail}>
-          <Input
-            id="email"
-            type="email"
-            placeholder="Email"
-            isRequired
-            value={emailInput}
-            onChange={(e) => setEmailInput(e.target.value)}
-          />
-          <FormErrorMessage>Please enter a valid email address.</FormErrorMessage>
-        </FormControl>
-        <Textarea id="emailbody" isRequired value={bodyInput} onChange={(e) => setBodyInput(e.target.value)} />
-
-        <HStack justify="flex-end">
-          <Button variant="outline" onClick={handleCancel}>
-            Cancel
-          </Button>
-          <Button type="submit" colorScheme="green">
-            Submit
-          </Button>
-        </HStack>
-      </Stack>
-    </form>
+    <Popover isOpen={isOpen} onOpen={onOpen} onClose={onClose} closeOnBlur={false}>
+      <PopoverTrigger>
+        <Button width={75} height={75} variant="ghost" colorScheme="green" border="1px" borderRadius="100%">
+          <Icon as={EnvelopeClosedIcon} h={50} w={50} />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent p={5} w={{ md: 500 }}>
+        <PopoverArrow />
+        <PopoverCloseButton />
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={4} p={2}>
+            <Input
+              id="name"
+              placeholder="Name"
+              isRequired
+              value={nameInput}
+              onChange={(e) => setNameInput(e.target.value)}
+            />
+            <FormControl isInvalid={!validEmail}>
+              <Input
+                id="email"
+                type="email"
+                placeholder="Email"
+                isRequired
+                value={emailInput}
+                onChange={(e) => setEmailInput(e.target.value)}
+              />
+              <FormErrorMessage>Please enter a valid email address.</FormErrorMessage>
+            </FormControl>
+            <Textarea id="emailbody" isRequired value={bodyInput} onChange={(e) => setBodyInput(e.target.value)} />
+            <HStack justify="flex-end">
+              <Button variant="outline" onClick={handleCancel}>
+                Cancel
+              </Button>
+              <Button type="submit" colorScheme="green" isLoading={emailSending}>
+                Submit
+              </Button>
+            </HStack>
+          </Stack>
+        </form>
+      </PopoverContent>
+    </Popover>
   );
 }
